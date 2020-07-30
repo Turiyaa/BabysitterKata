ad1e89525bdabfd9351e62a961b7a67b4b1198eb
Turiyaa <nneopane@oswego.edu>
2020-07-30 16:25:50 -0400

    Adding more Junit case to cover exception and time constarint

--- src/main/java/com/babysitterkata/service/Payment.java
+++ src/main/java/com/babysitterkata/service/Payment.java
@@ -17,54 +17,54 @@ public class Payment {
 		LocalTime st = parseToMilitary(startTime);
 		LocalTime bt = parseToMilitary(bedTime);
 		LocalTime et = parseToMilitary(endTime);
-		
+
 		boolean isValid = validateInput(st, bt, et);
-		
-		if(isValid) {
-			
+
+		if (isValid) {
+
 			babysitter = new Babysitter();
-			
+
 			Duration start_time_hours = Duration.between(st, bt);
 			Duration bed_time_hours = Duration.between(bt, (LocalTime.MAX));
 			Duration midnight_time_hours = Duration.between(LocalTime.MIDNIGHT, et);
-			
-			babysitter.setStartHour((int)start_time_hours.toHours());
-			babysitter.setBedTimeHour((int)bed_time_hours.toHours()+1); // Adding 1 to fix lost hou
-			babysitter.setMidnightHour((int)midnight_time_hours.toHours());
-			
+
+			babysitter.setStartHour((int) start_time_hours.toHours());
+			babysitter.setBedTimeHour((int) bed_time_hours.toHours() + 1); // Adding 1 to fix lost hour
+			babysitter.setMidnightHour((int) midnight_time_hours.toHours());
+
 			charge = calculateCharge(charge);
 
 		}
-		
+
 		return charge;
-		
+
 	}
 
 	private double calculateCharge(double charge) {
-		charge = babysitter.getStartHour() * BabysitterConst.STARTTIME_PAY +
-				 babysitter.getBedTimeHour() * BabysitterConst.BEDTIME_PAY +
-				 babysitter.getMidnightHour() * BabysitterConst.MIDNIGHT_PAY;
+		charge = babysitter.getStartHour() * BabysitterConst.STARTTIME_PAY
+				+ babysitter.getBedTimeHour() * BabysitterConst.BEDTIME_PAY
+				+ babysitter.getMidnightHour() * BabysitterConst.MIDNIGHT_PAY;
 		return charge;
 	}
-	
+
 	public boolean validateInput(LocalTime st, LocalTime bt, LocalTime et) throws BabySitterKataException {
 
-		if(st.isBefore(BabysitterConst.START_TIME)) {
+		if (st.isBefore(BabysitterConst.START_TIME)) {
 			throw new BabySitterKataException("Start time can't be earlier then 5:00 PM");
 		}
-		if(st.isAfter(bt)) {
+		if (st.isAfter(bt)) {
 			throw new BabySitterKataException("Start time can't be after bedtime");
 		}
-		if(et.isAfter(BabysitterConst.END_TIME)) {
+		if (et.isAfter(BabysitterConst.END_TIME)) {
 			throw new BabySitterKataException("End time can't be after 4:00AM");
 		}
 		return true;
 	}
-	
+
 	public LocalTime parseToMilitary(String time) {
-		String localTime = LocalTime.parse(time, DateTimeFormatter.ofPattern("hh:mm a")).format(DateTimeFormatter.ofPattern("HH:mm"));
-		
+		String localTime = LocalTime.parse(time, DateTimeFormatter.ofPattern("hh:mm a"))
+				.format(DateTimeFormatter.ofPattern("HH:mm"));
 		return LocalTime.parse(localTime);
 	}
-	
+
 }
--- src/test/java/com/babysitterkata/service/PaymentTest.java
+++ src/test/java/com/babysitterkata/service/PaymentTest.java
@@ -14,16 +14,6 @@ public class PaymentTest {
 	private static final double DELTA = 0;
 	
 	@Test
-	public void testValidation() throws BabySitterKataException {
-		payment = new Payment();
-		LocalTime st = LocalTime.parse("17:00");
-		LocalTime bt = LocalTime.parse("18:00");
-		LocalTime et = LocalTime.parse("04:00");
-		
-		assertEquals(true, payment.validateInput(st, bt, et));
-	}
-	
-	@Test
 	public void testCalculateCharge() throws BabySitterKataException {
 		payment = new Payment();
 		assertEquals(132, payment.calculateCharge("05:00 PM","08:00 PM", "04:00 AM"), DELTA);
@@ -34,5 +24,33 @@ public class PaymentTest {
 		payment = new Payment();
 		assertEquals(112, payment.calculateCharge("06:00 PM","10:00 PM", "03:00 AM"), DELTA);
 	}
+	
+	@Test(expected = BabySitterKataException.class)
+	public void testCalculateChargeStartTime() throws BabySitterKataException {
+		payment = new Payment();
+		assertEquals(112, payment.calculateCharge("03:00 PM","10:00 PM", "03:00 AM"), DELTA);
+	}
 
+	@Test(expected = BabySitterKataException.class)
+	public void testCalculateChargeEndTime() throws BabySitterKataException {
+		payment = new Payment();
+		assertEquals(112, payment.calculateCharge("05:00 PM","10:00 PM", "05:00 AM"), DELTA);
+	}
+	
+	@Test
+	public void testParseToMilitary() {
+		payment = new Payment();
+		LocalTime expected = LocalTime.parse("17:00");
+		assertEquals(expected, payment.parseToMilitary("05:00 PM"));
+	}
+	
+	@Test
+	public void testValidation() throws BabySitterKataException {
+		payment = new Payment();
+		LocalTime st = LocalTime.parse("17:00");
+		LocalTime bt = LocalTime.parse("18:00");
+		LocalTime et = LocalTime.parse("04:00");
+		
+		assertEquals(true, payment.validateInput(st, bt, et));
+	}
 }
